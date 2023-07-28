fe0dcfaabee392ce49113a8a616707336c620b66
johnc <john.chen@unisco.com>
2023-07-28 16:27:23 +0800

    新增routeId & driverId,  提供报表结果的get接口

--- lastmile/lastmile.js
+++ lastmile/lastmile.js
@@ -4,10 +4,12 @@ response = {
     datas:[
         {
             route: "190",
+            routeId: "7bc71186-9d6f-4541-a1b3-ffcfe0b6234f",
             status: "checked, uncheck, checking",//考虑多个司机的情况下， 全部检查， 没开始检查， 任意一个司机被检查
             drivers: [
                 {
                     driver: "Mr.A",
+                    driverId: "9d0b5ee9-fa02-4d6e-8633-f7980b225ae1",
                     bScanedQty: 10,
                     excep: {
                         overage: 1,
@@ -34,9 +36,11 @@ response = {
     datas:[
         {
             driver: "Mr.A",
+            driverId: "9d0b5ee9-fa02-4d6e-8633-f7980b225ae1",
             status: "checked, uncheck",
             routes: [{
                 route: "190",
+                rouuteId: "7bc71186-9d6f-4541-a1b3-ffcfe0b6234f",
                 bScanedQty: 10,
                 excep: {
                     overage: 1,
@@ -58,22 +62,36 @@ response = {
 
 //post /lso/qc_scan_result/latest-get
 request = {
-    assigneeDriver: 'Mr.A',
-    assigneeRoutes: ["190", "191"],
+    assigneeDriverId: '9d0b5ee9-fa02-4d6e-8633-f7980b225ae1',
+    assigneeRouteIds: ["7bc71186-9d6f-4541-a1b3-ffcfe0b6234f"],
 }
 response = {qcId: '131654411'}
 
 //post /lso/qc-scan-result/generate  (生成一个qc记录， 手机端取qcId 用于绑定qc 扫描上来的包裹)
 request = {
-    assigneeDriver: 'Mr.A',
-    assigneeRoutes: ["190", "191"],
+    assigneeDriverId: '9d0b5ee9-fa02-4d6e-8633-f7980b225ae1',
+    assigneeRouteIds: ["7bc71186-9d6f-4541-a1b3-ffcfe0b6234f"],
 }
 response = {qcId: '131654411'}
+
+//get  /lso/qc-scan-result/:qcId
+response =  {
+    _id: "qcId",
+    driver: 'MR.A',
+    driverId: "xxxx",
+    routes:["190", "191"],
+    routeIds: ["xxxx", "xxxx"],
+    status: "New, Close",
+    driverAssigneeQty: 100,
+    shortQty: 2,
+    overageQty: 2,
+    driverSigatureFileId: 'xxxx',
+}
 
 
 //get /lso/qc-scan-result/:qcId/in-progress
 response = {
-    driverAssigneePkgs: [{sequence: 1,driver: 'Mr.A',trackingNo: '1234567',route: '190'}],
+    driverAssigneePkgs: [{sequence: 1,driver: 'Mr.A', driverId: "xxxxx", trackingNo: '1234567',route: '190', routeId: "xxxx"}],
     qcScanPkgs:[{qcId: '165456789915', trackingNo: '1234567', status: 'scanned'}]
 }
 
@@ -87,8 +105,8 @@ request = {
 3
 //post /lso/qc-scan/:qcId/analysis
 response = {
-    excepPkgs: [{_id: "123465498798", route: 'a', sequence: '1', trackingNO: '78974613', driver: 'Mr.A'}],
-    result: [{_id: "123465498798", route: 'a', sequence: '1', trackingNO: '78974613', driver: 'Mr.A', status: "Overage, Short"}] // trackingNo 是一定会有的， 它来源于两部分， driver_assignee & qc_scan,   overage 只存在qc_scan 有， 但是driver_assignee没有, short=> qc_scan没有 或者 driver_scan 没有
+    excepPkgs: [{_id: "123465498798", route: 'a', routeId: "xxxxx", sequence: '1', trackingNO: '78974613', driver: 'Mr.A', driverId: "xxxxx"}],
+    result: [{_id: "123465498798", route: 'a', routeId: "xxxxx", sequence: '1', trackingNO: '78974613', driver: 'Mr.A', driverId: "xxxxx", status: "Overage, Short"}] // trackingNo 是一定会有的， 它来源于两部分， driver_assignee & qc_scan,   overage 只存在qc_scan 有， 但是driver_assignee没有, short=> qc_scan没有 或者 driver_scan 没有
 }
 
 //delete /lso/qc-scan-data/:qcId/trackingNo/:trackingNo
@@ -128,8 +146,8 @@ request = {
 //         routes:["190", "191"],
 //         status: "New, Close"
 //     driverAssigneeQty: 100,
-//         short: 2,
-//         overage: 2,
+//         shortQty: 2,
+//         overageQty: 2,
 //         driverSigatureFileId: 'xxxx',
 // }
 //
